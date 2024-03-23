ea30957555f9b288c24a8658fd7f85b2f6e724f1
Waigwa254 <augustinewaigwa254@gmail.com>
2024-03-23 13:39:02 -0700

    puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
-# using puppet to make changes to our configuration file
+# Atomating my Tasks using Puppet
 
-file { 'ect/ssh/ssh_cofig':
-        ensure => present,
-
-content =>"
-
-        #SSH client configuration
-        host*
-        IdentityFile ~/.ssh/school
-        PasswordAuthentication no
-",
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+content => "
+    # SSH client configuration
+    Host *
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+  ",
 }
