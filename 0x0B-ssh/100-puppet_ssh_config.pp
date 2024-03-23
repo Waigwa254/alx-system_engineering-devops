4b0175a162ac35e5ee0474f365479f18b1dd0f8e
Waigwa254 <augustinewaigwa254@gmail.com>
2024-03-23 13:22:15 -0700

    pppt

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env pasn
+# using puppet to make changes to our configuration file
+file {
+'ect/ssh/ssh_cofig':
+ensure => present,
+content =>"
+#SSH client configuration
+host*
+IdentityFile ~/.ssh/school
+PasswordAuthentication
+"
+}
